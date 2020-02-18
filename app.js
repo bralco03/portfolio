a2f00a63d6bb476a5f4e2e88e48996dd60ba7495
Bryan Correa <bralco03@hotmail.com>
2020-02-17 17:17:34 -0700

    placed some styling when the submit button is pressed.

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const Form = document.querySelector('#form1');
 const formName = document.querySelector('#name');
 const formEmail = document.querySelector('#email');
 const formMessage = document.querySelector('#message');
-const formSub = document.querySelector('#sub');
+const submitSuccess = document.querySelector('.success');
 
 Form.addEventListener('submit', e => {
     setInterval(function(){
         document.getElementById("form1").reset();
-        formSub.value = "MESSAGE SENT";
+        submitSuccess.innerText= "Message Sent"
     }, 3000);
 });
