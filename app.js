44ea1fb4aa8a3cbbe72302ee14e3ecb4b2e445f1
Bryan Correa <bralco03@hotmail.com>
2020-02-19 22:00:04 -0700

    site is now functional and ready to go. Version 1.0 is now done.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const formName = document.querySelector('#name');
 const formEmail = document.querySelector('#email');
 const formMessage = document.querySelector('#message');
 const submitSuccess = document.querySelector('.success');
+const navlinks = document.querySelectorAll('.navigation li');
+
 
 Form.addEventListener('submit', e => {
     setInterval(function(){
@@ -10,3 +12,26 @@ Form.addEventListener('submit', e => {
         submitSuccess.innerText= "Message Sent"
     }, 3000);
 });
+const navSlide = () => {
+    const burguer = document.querySelector('.hamburguer');
+    const nav = document.querySelector('.navigation');
+     
+        burguer.addEventListener('click', () => {
+         nav.classList.toggle('nav-active');
+         navlinks.forEach((link,index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 1}s`;
+            }
+         });
+         burguer.classList.toggle('toggle');
+        });
+
+    
+};
+navSlide();
+
+
+
+
